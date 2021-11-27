4ca0852a397aad07b7ea09187b40571b2e59f00e
TheMatthewHarris <mattshane132@gmail.com>
2021-11-27 15:54:23 -0500

    Initial implementation of Socket.io

--- frontend/src/server/index.js
+++
@@ -1,6 +0,0 @@
-const httpServer = require("http").createServer();
-const io = require("socket.io")(httpServer, {  
-    cors: {    
-        origin: "http://localhost:8080",  
-    },
-});
--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,6 +1,6 @@
-import { io } from "sockjs-client";
+import { io } from "socket.io-client";
 
-const URL = "http://localhost:3000";
-const socket = io(URL, { autoConnect: false });
+const URL = "http://localhost:4000";
+const socket = io(URL, { autoConnect: true });
 
 export default socket;
