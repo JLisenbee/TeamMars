7576957cdd13dfdbfdd16f5852c569c7e283aad1
Aman Singh <amanssingh007@gmail.com>
2021-12-01 20:55:40 -0500

    updated groupChat.js

--- frontend/src/components/GroupChat.js
+++ frontend/src/components/GroupChat.js
@@ -1,40 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useStoreState } from 'easy-peasy'
 import io from "socket.io-client"
-import { v4 as uuidv4 } from 'uuid';
 import "../css/groups.css";
 
-var displayname = "DScrute"; // TODO: Fetch username from backend
+
+const socket = io('http://localhost:5000');
 
 const GroupChat = (props) => {
     
-    const initialChat = [
-        {username: '', text: ''}
-    ];
+    const userName = useStoreState((states) => states.userName)
+    
+    const [chat, setChat] = useState([]);
+    const [msg, setMsg] = useState({username: userName, text: ''});
+    
+    
+    useEffect(() => {
+        socket.on('newMsg', (added) => {
+            const update = [...chat, {username: added.username, text: added.text}];
+            setChat(update);
+        });
+    })
 
-    const [chat, setChat] = React.useState(initialChat);
-    const [msg, setMsg] = React.useState({username: displayname, text: ''});
+    
 
-    var socket = io('http://localhost:4000', { transports : ['websocket'] });
 
-function handleChange(event) {
-    setMsg({...msg, [event.target.name]: event.target.value});
-}
-
-function handleAdd() {
-    if (msg.text == '') {
-        return;
+    function handleChange(event) {
+        setMsg({...msg, [event.target.name]: event.target.value});
     }
 
-    const added = {username: msg.username, text: msg.text};
-    socket.emit('new msg', added);
-    setMsg({username: msg.displayname, text: ''})
-}
+    function handleAdd() {
+        if (msg.text.length !== 0) {
+            const added = {username: msg.username, text: msg.text};
+            socket.emit('newMsg', added);
+            setMsg({username: msg.displayname, text: ''})
+        }
+    }
 
 
-socket.on('new msg', (added) => {
-    const update = [...chat, {username: added.username, text: added.text}];
-    setChat(update);
-  });
 
 
     return(
@@ -42,8 +44,8 @@ socket.on('new msg', (added) => {
         <div className = "box">
             <div className = "chatBox">
                 <ul className = "chatStyle">
-                {chat.slice(1).map((item) => (
-                    <li key={uuidv4()}><span>{item.username}<br /></span>{item.text}</li>
+                {chat.slice(1).map((item, key) => (
+                    <li key={key}><span>{item.username}<br /></span>{item.text}</li>
                 ))}
                 </ul>
             </div>
