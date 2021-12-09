bf687720b85be3c54cbf6741a557d652e463ed49
Aman Singh <amanssingh007@gmail.com>
2021-12-08 20:12:42 -0500

    user profile link added

--- frontend/src/components/FriendList.js
+++ frontend/src/components/FriendList.js
@@ -1,16 +1,30 @@
+import { useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
+import variable from '../variable'
 import Friend from './Friend'
 
 // Header for pages, contains the logo with link to home
 // As well as a search bar and various buttons
 const FriendList = () => {
+
+    useEffect(() => {
+        axios({
+            method: "GET",
+            withCredentials: true,
+            url: variable.SERVER_TOKEN_CHECK,
+          }).then((res) => {
+          })
+          .catch((error) => {
+          })
+    }, [])
+
     return(
-        <div>
+        <div style={{ background: 'red', display: 'flex', width: '100%', flexDirection: 'column', alignItems: 'flex-start' }}>
             <h2>Friends Online</h2>
-            <Friend />
-            <Friend />
-            <Friend />
-            <Friend />
+            <div style={{ background: 'blue', display: 'flex', width: '100%', flexDirection: 'column', alignItems: 'flex-start'}}>
+                <Friend/>
+            </div>
         </div>
     )
   }
